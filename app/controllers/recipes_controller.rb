ab5f217986316fb8987cdd6174ae4a2ab8dd50a4
anghanvaishu <anghanvaishu@github.com>
2017-08-04 18:04:48 +0530

    Recipe Module Completed

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
   before_action :find_recipe,only: [:show,:edit,:update,:destroy]
   def index
+    @recipe=Recipe.all.order("created_at DESC")
   end
 
   def show
@@ -20,6 +21,22 @@ class RecipesController < ApplicationController
     end
   end
 
+  def edit
+
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to root_path,notice: "Successfully deleted recipe"
+  end
   private
 
   def recipe_params
