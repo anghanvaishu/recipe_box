305f285e616a4332f5e6b55d86c33d846352739a
anghanvaishu <anghanvaishu@github.com>
2017-08-09 19:24:51 +0530

    Cocoon udated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require rails-ujs
+//= require cocoon
 document.addEventListener('turbolinks:load', Bootsy.init);
 //= require_tree .
-//= require cocoon
+
